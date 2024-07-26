4cb778cb96e3f188ef747eb02f973fe0977b4726
sydneyowl <84191144+SydneyOwl@users.noreply.github.com>
2024-07-26 12:28:29 +0800

    :bookmark: Ready for v0.3.3

--- Constants/Common/TIPS.cs
+++ Constants/Common/TIPS.cs
@@ -7,7 +7,7 @@ public class TIPS
 {
     public static List<string> TipList = new()
     {
-        "Tips: 对信道进行拖动或清空等操作后序号可能会出现“27”，此时上下拉动右侧滚动条即可！",
+        "Tips: 对信道进行拖动或清空等操作后序号可能会出现序号异常或字段被清空等问题，此时上下拉动右侧滚动条刷新界面即可！",
         "Tips: 如遇到任何问题，欢迎在github中提出issue或PR!",
         $"Tips: 软件版本：{(Properties.Version.VersionTag == "@TAG_NAME@" ? "（内部版本）" : Properties.Version.VersionTag)}，可在github上检查、获取更新！",
         "Tips: 体谅、忠诚、进步、友爱、适度、爱国",
